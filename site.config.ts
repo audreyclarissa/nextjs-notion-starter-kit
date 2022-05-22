27458bb924b639d267eae02b13187f9333081d48
audreyclarissa <99718904+audreyclarissa@users.noreply.github.com>
2022-05-22 18:49:27 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9de1858477b143a18ef034bf29e7eba7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Audrey Clarissa',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Audrey Clarissa',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
